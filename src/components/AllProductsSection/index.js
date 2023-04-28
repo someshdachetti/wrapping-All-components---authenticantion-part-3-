b2ba98023f7491dde6078e8d38cd5c6d3d20fd28
someshdachetti <dssomesh201@gmail.com>
2023-04-28 05:43:36 +0000

    main

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -1,11 +1,39 @@
 import {Component} from 'react'
-
+import Cookies from 'js-cookie'
 import ProductCard from '../ProductCard'
 import './index.css'
 
 class AllProductsSection extends Component {
   state = {
     productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtxxxToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtxxxToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.map(each => ({
+        title: each.title,
+        id: each.id,
+        imageUrl: each.image_url,
+        rating: each.rating,
+        price: each.price,
+        brand: each.brand,
+      }))
+      this.setState({productsList: updatedData})
+    }
   }
 
   renderProductsList = () => {
